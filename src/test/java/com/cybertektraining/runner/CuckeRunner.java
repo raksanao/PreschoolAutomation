370349113306b6a018ae46e176941711053ef0f0
Rukhshona Olimova <rukhshona87@gmail.com>
2020-10-06 00:12:42 -0500

    first commit

---
+++ src/test/java/com/cybertektraining/runner/CuckeRunner.java
@@ -0,0 +1,4 @@
+package com.cybertektraining.runner;
+
+public class CuckeRunner {
+}
---
+++ src/test/java/com/cybertektraining/step_defenitions/CreateStudentStepDefenition.java
@@ -0,0 +1,4 @@
+package com.cybertektraining.step_defenitions;
+
+public class CreateStudentStepDefenition {
+}
